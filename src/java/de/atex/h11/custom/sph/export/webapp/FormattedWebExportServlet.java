dbba90e5894e0bfb335dbc54decab09e0d7e0632
jp <jmariano@atex.com>
2014-09-10 16:30:54 +0800

    correction related to commas

--- src/java/de/atex/h11/custom/sph/export/webapp/FormattedWebExportServlet.java
+++ src/java/de/atex/h11/custom/sph/export/webapp/FormattedWebExportServlet.java
@@ -1186,6 +1186,7 @@ public class FormattedWebExportServlet extends HttpServlet {
             nl = (NodeList) xp.evaluate("//body/title", doc, XPathConstants.NODESET);
             if (nl.getLength() == 1) {
                 strTitle = replaceReservedCharMarkers(strTitle);
+                strTitle = strTitle.replaceAll("\\ *,\\ *", ",");
                 ((Element) nl.item(0)).setTextContent(strTitle.trim());
             }
         }
@@ -1197,6 +1198,7 @@ public class FormattedWebExportServlet extends HttpServlet {
             nl = (NodeList) xp.evaluate("//body/country", doc, XPathConstants.NODESET);
             if (nl.getLength() == 1) {
                 strPlace = replaceReservedCharMarkers(strPlace);
+                strPlace = strPlace.replaceAll("\\ *,\\ *", ",");
                 ((Element) nl.item(0)).setTextContent(strPlace.trim());
             }
         }
@@ -1208,6 +1210,7 @@ public class FormattedWebExportServlet extends HttpServlet {
             nl = (NodeList) xp.evaluate("//body/series", doc, XPathConstants.NODESET);
             if (nl.getLength() == 1) {
                 strEmail = replaceReservedCharMarkers(strEmail);
+                strEmail = strEmail.replaceAll("\\ *,\\ *", ",");
                 ((Element) nl.item(0)).setTextContent(strEmail.trim());
             }
         }
@@ -1219,6 +1222,7 @@ public class FormattedWebExportServlet extends HttpServlet {
             nl = (NodeList) xp.evaluate("//body/twitter", doc, XPathConstants.NODESET);
             if (nl.getLength() == 1) {
                 strTwitter = replaceReservedCharMarkers(strTwitter);
+                strTwitter = strTwitter.replaceAll("\\ *,\\ *", ",");
                 ((Element) nl.item(0)).setTextContent(strTwitter.trim());
             }
         }       
